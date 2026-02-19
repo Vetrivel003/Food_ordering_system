815e5e768fc9ffc66a1224285ba49c3e6794cf41
Vetrivel A <vetrivel.a2022ai-ds@sece.ac.in>
2026-02-19 11:13:51 +0530

    feat: implement CustomUserDetails for Spring Security

---
+++ src/main/java/com/project/sapaadu/security/CustomUserDetails.java
@@ -0,0 +1,56 @@
+package com.project.sapaadu.security;
+
+import com.project.sapaadu.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+public class CustomUserDetails implements UserDetails {
+
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+
+        return user.getUserRoles()
+                .stream()
+                .map(userRole ->
+                        (GrantedAuthority) () -> "ROLE_" + userRole.getRole().getName()
+                )
+                .collect(Collectors.toSet());
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPasswordHash();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return !user.isBlocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
